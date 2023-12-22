f8c6dd730cef7964a31fd6d434a696aaa7fd486d
it-mnm <noble3769@gmail.com>
2023-12-22 18:36:13 +0900

    [ADD} - pickle 로직추가

--- dags/pickle.py
+++ dags/pickle.py
@@ -1,29 +1,55 @@
-import pickle
+import csv
+import json
+import pandas as pd
+import numpy as np
+import logging
+import pendulum
+from datetime import datetime, timedelta
+from tempfile import NamedTemporaryFile
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.hooks.mysql_hook import MySqlHook
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+from surprise import SVD, Reader, Dataset, accuracy
+from lightfm import LightFM, cross_validation
+from lightfm.data import Dataset
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import coo_matrix
 
-def read_pickle_from_s3(bucket_name, key):
+
+# 기본 설정
+default_args = {
+    'owner': 'admin',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=10)
+}
+
+local_tz = pendulum.timezone("Asia/Seoul")
+
+
+
+# 피클 파일로 S3에 모델 업데이트
+def model_update():
     s3_hook = S3Hook(aws_conn_id='aws_default')
 
-    try:
-        # S3에서 Pickle 파일 다운로드
-        pickle_content = s3_hook.read_key(key, bucket_name)
+    # S3에 파일이 존재하는지 확인
+    s3_hook.check_for_key('model_genre.pkl', 'hello00.net-model')
 
-        # Pickle 파일 읽기
-        data = pickle.loads(pickle_content)
+    logging.info("*****pickle 파일 읽어오기*****")
 
-        return data
-    except Exception as e:
-        print(f"Error reading Pickle file from S3: {e}")
-        return None
 
-# 사용 예시
-bucket_name = 'your_s3_bucket_name'
-pickle_key = 'your/pickle/file/path.pickle'
+# DAG 설정
+with DAG(
+    dag_id="pickle",
+    default_args=default_args,
+    start_date=datetime(2023, 12, 11, tzinfo=local_tz),
+    schedule_interval='@once'
+) as dag:
+    pickle =PythonOperator(
+        task_id="model_update",
+        python_callable=model_update
+    )
+    pickle
 
-pickle_data = read_pickle_from_s3(bucket_name, pickle_key)
-
-if pickle_data:
-    print("Pickle file contents:")
-    print(pickle_data)
-else:
-    print("Failed to read Pickle file from S3.")
--- dags/vod_rec3.py
+++ dags/vod_rec3.py
@@ -1,13 +1,10 @@
-# RDS 데이터를 가져와 전처리 수행 후 머신러닝 적용 및 성능평가
-# JSON 파일로 S3에 저장
-# 성능 데이터 적재
-
 import csv
 import json
 import pandas as pd
 import numpy as np
 import logging
 import pendulum
+import pickle
 from datetime import datetime, timedelta
 from tempfile import NamedTemporaryFile
 from airflow import DAG
@@ -20,6 +17,7 @@ from surprise import SVD, Reader, Dataset, accuracy
 from lightfm import LightFM, cross_validation
 from lightfm.data import Dataset
 from sklearn.metrics.pairwise import cosine_similarity
+from scipy.sparse import coo_matrix
 
 
 # 기본 설정
@@ -43,7 +41,7 @@ def week_info_s3(**context):
     #파일이 존재하지 않을 경우 새 파일 생성, 존재할 경우에는 읽어오기
     if not file_exists:
         logging.info("*****파일 존재하지 않으므로 새로생성*****")
-        new_json = {"columns": ["week_info"],"values": [31]}
+        new_json = {"columns": ["week_info"],"values": [40]}
         updated_json_data = json.dumps(new_json, indent=2)
         current_week=new_json["values"][0]
         s3_hook.load_string(updated_json_data, 'week_info/week_info.json', 'hello00.net-airflow', replace=True)
@@ -53,10 +51,10 @@ def week_info_s3(**context):
         existing_data = s3_hook.read_key('week_info/week_info.json', 'hello00.net-airflow')
         existing_json = json.loads(existing_data)
         #이전 주차 정보 읽기
-        current_week=existing_json["values"][0]
+        current_week=existing_json["values"][0] + 1
 
         #기존 주차 정보 업데이트
-        existing_json["values"] = [current_week + 1]
+        existing_json["values"] = [current_week]
         updated_json = json.dumps(existing_json, indent=2)
         s3_hook.load_string(updated_json, 'week_info/week_info.json', 'hello00.net-airflow', replace=True)
     
@@ -80,9 +78,14 @@ def mysql_hook(**context):
 
 
     users = pd.read_sql('select * from userinfo', connection)
-    vods = pd.read_sql(f'select * from vods_sumut where week(log_dt)={current_week}', connection)
-    conts = pd.read_sql(f'select * from contlog where week(log_dt)={current_week}', connection)
+    vods = pd.read_sql(f'select * from vods_sumut where week(log_dt)<={current_week}', connection)
+    conts = pd.read_sql(f'select * from contlog where week(log_dt)<={current_week}', connection)
     program_info_all = pd.read_sql('select * from vodinfo', connection)
+
+    logging.info(users)
+    logging.info(vods)
+    logging.info(conts)
+    logging.info(program_info_all)
 
     context["task_instance"].xcom_push(key="users", value=users)
     context["task_instance"].xcom_push(key="vods", value=vods)
@@ -98,76 +101,98 @@ def data_preprocessing(**context):
     logging.info("데이터 전처리")
 
     # mysql_hook 함수에서 사용했던 변수 불러오기
-    users = context["task_instance"].xcom_pull(task_ids="data_query", key="users")
-    vods = context["task_instance"].xcom_pull(task_ids="data_query", key="vods")
-    conts = context["task_instance"].xcom_pull(task_ids="data_query", key="conts")
-    program_info_all = context["task_instance"].xcom_pull(task_ids="data_query", key="program_info_all")
+    users = context["task_instance"].xcom_pull(task_ids="mysql_hook", key="users")
+    vods = context["task_instance"].xcom_pull(task_ids="mysql_hook", key="vods")
+    conts = context["task_instance"].xcom_pull(task_ids="mysql_hook", key="conts")
+    program_info_all = context["task_instance"].xcom_pull(task_ids="mysql_hook", key="program_info_all")
 
 
+    # 전처리 시작
     # e_bool == 0 인 데이터만 뽑기
     vod_log = vods[vods['e_bool']==0][['subsr_id', 'program_id', 'program_name', 'episode_num', 'log_dt', 'use_tms', 'disp_rtm_sec', 'count_watch', 'month']]
     cont_log = conts[conts['e_bool']==0][['subsr_id', 'program_id', 'program_name', 'episode_num', 'log_dt', 'month']]
     vod_info = program_info_all[program_info_all['e_bool']==0][['program_id','program_name', 'ct_cl', 'program_genre', 'release_date', 'age_limit']]
     user_info = users.copy()
 
-    # use_tms / running time
     vod_log['use_tms_ratio'] = vod_log['use_tms'] / vod_log['disp_rtm_sec']
-    use_tms_ratio = vod_log.groupby(['subsr_id', 'program_id'])[['use_tms_ratio']].max().reset_index()
+    vod_log['log_dt'] = pd.to_datetime(vod_log['log_dt'])
+    cont_log['log_dt'] = pd.to_datetime(cont_log['log_dt'])
+    cont_log['recency'] = (cont_log['log_dt'].max() - cont_log['log_dt']).dt.days # 최근성
 
-    click_cnt = cont_log.groupby(['subsr_id', 'program_id'])[['log_dt']].count().reset_index().rename(columns={'log_dt':'click_cnt'})
+    log = pd.concat([vod_log[['subsr_id', 'program_id']], cont_log[['subsr_id', 'program_id']]]).drop_duplicates().reset_index(drop=True)
+    log = log.merge(cont_log.groupby(['subsr_id', 'program_id'])[['program_name']].count().reset_index().rename(columns={'program_name':'click_cnt'}), how='left')
+    log = log.merge(cont_log.groupby(['subsr_id', 'program_id'])[['recency']].min().reset_index().rename(columns={'recency':'click_recency'}), how='left')
 
-    # vod, cont 합치기
-    rating = pd.concat([vod_log[['subsr_id', 'program_id']], cont_log[['subsr_id', 'program_id']]]).drop_duplicates().reset_index(drop=True)
-    rating = rating.merge(use_tms_ratio, how='left').merge(click_cnt, how='left')
+    # (subsr_id, program_id) 쌍에 대해 하나의 평점만 남겨야 함
+    # => use_tms_ratio 는 최대값으로 남기고, 시청 count도 해서 추가한다.
+    log = log.merge(vod_log.groupby(['subsr_id', 'program_id'])[['use_tms_ratio']].max().reset_index().rename(columns={'use_tms_ratio':'watch_tms_max'}), how='left')
+    log = log.merge(vod_log.groupby(['subsr_id', 'program_id'])[['use_tms_ratio']].count().reset_index().rename(columns={'use_tms_ratio':'watch_cnt'}), how='left')
 
-    vod_info = vod_info.merge(rating.groupby('program_id').sum()[['click_cnt']].reset_index(), how='left')
-    rating = rating[['subsr_id', 'program_id', 'use_tms_ratio']]
+    replace_value = log['click_cnt'].quantile(0.95) # 95% 로 최대값 고정
+    log.loc[log[log['click_cnt'] > replace_value].index, 'click_cnt'] = replace_value
 
+    log['click_recency'] = log['click_recency'].max() - log['click_recency']
+
+
+    # 시청기록만 사용
+    replace_value = log['watch_cnt'].quantile(0.95)  # 95% 로 최대값 고정
+    log.loc[log[log['watch_cnt'] > replace_value].index, 'watch_cnt'] = replace_value
+
+    log_rating = log.copy()
+    log_rating['rating'] = log_rating['watch_tms_max']
+    log_rating.loc[log_rating[log_rating['rating']>0].index, '시청여부'] = 1
+    rating_df = log_rating[['subsr_id', 'program_id', 'rating', '시청여부']]
     vod_info['program_id'] = vod_info['program_id'].astype('int')
-    rating['program_id'] = rating['program_id'].astype('int')
+    rating_df['program_id'] = rating_df['program_id'].astype('int')
+    vod_info = vod_info.merge(log_rating.groupby(['program_id', 'subsr_id'])[['click_cnt']].count().groupby('program_id').count().reset_index(), how='left')
 
-    vod_info.loc[1743, 'program_genre'] = '드라마, 스릴러, 범죄, 가족'
-    vod_info.loc[4059, 'program_genre'] = '드라마, 가족'
-    vod_info.loc[3478, 'program_genre'] = '드라마, 로맨스'
-    vod_info.loc[3482, 'program_genre'] = '드라마, 로맨스, 코미디, 학교'
-    vod_info.loc[3487, 'program_genre'] = '드라마, 로맨스, 코미디'
-    vod_info.loc[4056, 'program_genre'] = '드라마, 액션'
-    vod_info.loc[3471, 'program_genre'] = '드라마, 코미디, 로맨스'
-    vod_info.loc[2706, 'program_genre'] = '드라마, 공포, 시대극'
+    max_subsr_id = rating_df['subsr_id'].max()
+    user_info = user_info[user_info['subsr_id'] < max_subsr_id]
+    
+    logging.info(vod_info)
+    logging.info(rating_df)
 
-    context["task_instance"].xcom_push(key="rating", value=rating)
+    context["task_instance"].xcom_push(key="rating_df", value=rating_df)
     context["task_instance"].xcom_push(key="vod_info", value=vod_info)
     context["task_instance"].xcom_push(key="user_info", value=user_info)
 
 
-# 모델 적용 및 성능평가 함수
+# 모델 적용 및 성능추출
 def model_running(**context):
-    rating = context["task_instance"].xcom_pull(task_ids="data_preprocessing", key="rating")
+    rating_df = context["task_instance"].xcom_pull(task_ids="data_preprocessing", key="rating_df")
     vod_info = context["task_instance"].xcom_pull(task_ids="data_preprocessing", key="vod_info")
     user_info = context["task_instance"].xcom_pull(task_ids="data_preprocessing", key="user_info")
     # 모델 함수
     logging.info("모델 적용 및 성능평가")
 
     class LightFM_Model:
-        def __init__(self, data, vod_info, user_info):
+        def __init__(self, data, vod_info, user_info, param_loss='logistic', param_components=10, param_epochs=10):
             self.vod_info = vod_info
             self.user_info = user_info
             self.train, self.test = self.split_evaluate(data)
             self.train_interactions, self.train_weights = self.dataset(self.train)
             self.score_matrix = self.create_score_matrix(data)
             self.score_matrix_evaluate = self.create_score_matrix(self.train)
+            ####
+            self.loss = param_loss
+            self.components = param_components
+            self.epoch = param_epochs
             self.precision, self.recall, self.map, self.mar, self.test_diversity, self.user_metrics = self.evaluate(self.train_interactions, self.train_weights, self.test)
             self.model = self.train_model(data)
             self.all_diversity = self.evaluate_all(self.model)
 
         def split_evaluate(self, data):
-            train, test = train_test_split(data.dropna(), test_size=0.25, random_state=0)
-            train = data.copy()
-            train.loc[test.index, 'use_tms_ratio'] = np.nan
+            #### 수정 완료
+            train, test = train_test_split(data[data['시청여부'].notnull()][['subsr_id', 'program_id', 'rating']], test_size=0.25, random_state=0)
+            train = data[['subsr_id', 'program_id', 'rating', '시청여부']].copy()
+            train.loc[test.index, 'rating'] = np.nan
             return train, test
 
+        #### 수정 완료
         def create_score_matrix(self, data):
-            score_matrix = data.pivot(columns='program_id', index='subsr_id', values='use_tms_ratio')
+            df = data.copy()
+            df.loc[df[(df['시청여부']==1) & df['rating'].notnull()].index, 'score_matrix'] = 1
+            score_matrix = df.pivot(columns='program_id', index='subsr_id', values='score_matrix')
             return score_matrix
 
         def dataset(self, train):
@@ -175,13 +200,17 @@ def model_running(**context):
             dataset.fit(users = train['subsr_id'].sort_values().unique(),
                         items = train['program_id'].sort_values().unique())
             num_users, num_vods = dataset.interactions_shape()
-            (train_interactions, train_weights) = dataset.build_interactions(train.dropna().values)
-            # (test_interactions, test_weights) = dataset.build_interactions(test.values)
+            ####
+            train['interaction'] = train['rating'].apply(lambda x: 0 if np.isnan(x) else 1)
+            train_dropna = train.dropna()
+            train_interactions = coo_matrix((train_dropna['interaction'], (train_dropna['subsr_id'], train_dropna['program_id'])), shape=(train['subsr_id'].max() + 1, train['program_id'].max() +1))
+            train_weights = coo_matrix((train_dropna['rating'], (train_dropna['subsr_id'], train_dropna['program_id'])), shape=(train['subsr_id'].max() + 1, train['program_id'].max() +1))
+            #(train_interactions, train_weights) = dataset.build_interactions(train[['subsr_id', 'program_id', 'rating']].dropna().values)
             return train_interactions, train_weights
 
-        def fit(self, fitting_interactions, fitting_weights, epochs=10):
-            model = LightFM(random_state=0)
-            model.fit(interactions=fitting_interactions, sample_weight=fitting_weights, verbose=1, epochs=epochs)
+        def fit(self, fitting_interactions, fitting_weights):
+            model = LightFM(random_state=0, loss=self.loss, no_components=self.components)
+            model.fit(interactions=fitting_interactions, sample_weight=fitting_weights, verbose=1, epochs=self.epoch)
             return model
 
         def predict(self, subsr_id, program_id, model):
@@ -279,12 +308,18 @@ def model_running(**context):
             dataset.fit(users = data['subsr_id'].sort_values().unique(),
                         items = data['program_id'].sort_values().unique())
             num_users, num_vods = dataset.interactions_shape()
-            (train_interactions, train_weights) = dataset.build_interactions(data.dropna().values)
+            ####
+            data['interaction'] = data['rating'].apply(lambda x: 0 if np.isnan(x) else 1)
+            data_dropna = data.dropna()
+            train_interactions = coo_matrix((data_dropna['interaction'], (data_dropna['subsr_id'], data_dropna['program_id'])))
+            train_weights = coo_matrix((data_dropna['rating'], (data_dropna['subsr_id'], data_dropna['program_id'])))
+            #(train_interactions, train_weights) = dataset.build_interactions(data[['subsr_id', 'program_id', 'rating']].dropna().values)
             # fitting
             model = self.fit(train_interactions, train_weights)
             return model
     # 모델 클래스 객체 생성하고, 성능 출력하는 코드
-    lfm_model = LightFM_Model(rating, vod_info, user_info)
+    lfm_model = LightFM_Model(rating_df, vod_info, user_info, 'warp', 30, 10)
+
 
     Precision = round(lfm_model.precision, 5)
     Recall = round(lfm_model.recall, 5)
@@ -299,49 +334,50 @@ def model_running(**context):
     context["task_instance"].xcom_push(key="MAR", value=MAR)
     context["task_instance"].xcom_push(key="test_Diversity", value=test_Diversity)
     context["task_instance"].xcom_push(key="all_Diversity", value=all_Diversity)
+    context["task_instance"].xcom_push(key="lfm_model", value=lfm_model)
 
 
-# JSON 파일로 변환 및 S3에 업로드 함수
+# 성능 S3에 적재
 def convert_to_json(**context):
     s3_hook = S3Hook(aws_conn_id='aws_default')
     current_week = context["task_instance"].xcom_pull(task_ids="week_info", key="current_week")
     current_year = datetime.now().year
     # 주차의 첫째날과 마지막날을 계산
-    first_day_of_week = datetime.strptime(f'{current_year}-W{current_week-1}-0', "%Y-W%W-%w").date()
-    last_day_of_week = first_day_of_week + timedelta(days=6)
-    current_week = f"{first_day_of_week.strftime('%Y.%m.%d')} ~ {last_day_of_week.strftime('%Y.%m.%d')}"
+    # first_day_of_week = datetime.strptime(f'{current_year}-W{current_week-1}-0', "%Y-W%W-%w").date()
+    # last_day_of_week = first_day_of_week + timedelta(days=6)
+    # current_week = f"{first_day_of_week.strftime('%Y.%m.%d')} ~ {last_day_of_week.strftime('%Y.%m.%d')}"
     logging.info(f"{current_week} 날짜 삽입")
 
     # Metrics to append
     metrics = [
-        {"name": "Precision", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="Precision")},
-        {"name": "Recall", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="Recall")},
-        {"name": "MAP", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="MAP")},
-        {"name": "MAR", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="MAR")},
-        {"name": "test_Diversity", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="test_Diversity")},
-        {"name": "all_Diversity", "value": context["task_instance"].xcom_pull(task_ids="model_running_and_create", key="all_Diversity")},
+        {"name": "Precision", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="Precision")},
+        {"name": "Recall", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="Recall")},
+        {"name": "MAP", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="MAP")},
+        {"name": "MAR", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="MAR")},
+        {"name": "test_Diversity", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="test_Diversity")},
+        {"name": "all_Diversity", "value": context["task_instance"].xcom_pull(task_ids="model_running", key="all_Diversity")},
     ]
 
 
 
     for metric in metrics:
-        metric_name = current_week
+        metric_name = metric["name"]
         metric_value = metric["value"]
 
         # S3에 파일이 존재하는지 확인
         file_exists = s3_hook.check_for_key(f'model_accuracy/{metric_name.lower()}.json', 'hello00.net-airflow')
-
-
+        
+        start_date = '2023.08.01'
         # 파일이 존재하지 않을 경우 새 파일 생성, 존재할 경우에는 기존 파일에 새로운 데이터 추가
         if not file_exists:
-            new_metrics = {"columns": [[metric_name]], "values": [[metric_value]]}
+            new_metrics = {"columns": [[f'{metric_name}({start_date})']], "values": [[metric_value]]}
             updated_json_data = json.dumps(new_metrics, indent=2)
         else:
             existing_data = s3_hook.read_key(f'model_accuracy/{metric_name.lower()}.json', 'hello00.net-airflow')
             existing_metrics = json.loads(existing_data)
 
             # 새로운 데이터 추가
-            existing_metrics["columns"] = [metric_name]
+            existing_metrics["columns"]=[[f'{metric_name}({start_date})']]
             existing_metrics["values"].append([metric_value])
 
             # Json으로 변환
@@ -351,29 +387,30 @@ def convert_to_json(**context):
         s3_hook.load_string(updated_json_data, f'model_accuracy/{metric_name.lower()}.json', 'hello00.net-airflow', replace=True)
 
 
-# 피클 파일 불러오기
-def pickle_load():
+# 피클 파일로 S3에 모델 업데이트
+def model_update(**context):
+    lfm_model = context["task_instance"].xcom_pull(task_ids="model_running", key="lfm_model")
+
+    pickle_file_path = 'model_lightfm.pkl'
+
+    with open('pickle_file_path', 'wb') as f:
+        pickle.dump(lfm_model, f)
+
     s3_hook = S3Hook(aws_conn_id='aws_default')
 
-    # S3에 파일이 존재하는지 확인
-    file_exists = s3_hook.check_for_key('model_genre.pkl', 'hello00.net-model')
+    s3_bucket_name = 'hello00.net-model'
+    s3_key = 'model_lightfm.pkl'
 
-    logging.info("*****pickle 파일 읽어오기*****")
-    logging.info(file_exists)
+    # S3에 피클 파일 업로드
+    s3_hook.load_file(
+        filename=pickle_file_path,
+        key=s3_key,
+        bucket_name=s3_bucket_name,
+        replace=True  # 기존 파일 덮어쓰기
+    )
 
-    existing_data = s3_hook.read_key('model_genre.pkl', 'hello00.net-model')
-    existing_metrics = json.loads(existing_data)
-    #이전 주차 정보 읽기
-    current_week=existing_metrics["values"][0]
 
-    #기존 주차 정보 업데이트
-    existing_metrics["values"] = [current_week + 1]
-    updated_json_data = json.dumps(existing_metrics, indent=2)
-    s3_hook.load_string(updated_json_data, 'week_info/week_info.json', 'hello00.net-airflow', replace=True)
-    
-    logging.info("*****현재 week 정보 받아옴*****")
 
-    logging.info(current_week)
 
 # DAG 설정
 with DAG(
@@ -386,21 +423,27 @@ with DAG(
     task_id="week_info",
     python_callable=week_info_s3
     )
-    data_query = PythonOperator(
-        task_id="data_query",
+    database = PythonOperator(
+        task_id="mysql_hook",
         python_callable=mysql_hook
     )
     data_preprocess = PythonOperator(
     task_id="data_preprocessing",
     python_callable=data_preprocessing
     )
-    model_run = PythonOperator(
-        task_id="model_running_and_create",
+    model_apply = PythonOperator(
+        task_id="model_running",
         python_callable=model_running
     )
-    upload_json_to_s3 =PythonOperator(
-        task_id="upload_to_s3",
+    dashboard_data_save = PythonOperator(
+        task_id="convert_to_json",
         python_callable=convert_to_json
     )
+    # pickle =PythonOperator(
+    #     task_id="model_update",
+    #     python_callable=pickle_load
+    # )
 
-    week_info >> data_query >> data_preprocess >> model_run >> upload_json_to_s3
+    week_info >> database >> data_preprocess >> model_apply >> dashboard_data_save
+    # week_info >> database >> data_preprocess >> model_apply >> pickle
+
